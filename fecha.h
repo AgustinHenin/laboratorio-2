ea2c62fc7de51b6d057440fc937315d9d3c34e53
fiorelopez <144172341+fiorelopez@users.noreply.github.com>
2024-06-06 17:09:44 -0300

    Update fecha.h

--- fecha.h
+++ fecha.h
@@ -34,4 +34,66 @@ public:
     int getmes() { return mes; }
     int getanio() { return anio; }
 
+//AGREGUE ESTE METODO
+std::string toString() {
+		std::string str = std::to_string(dia);
+		if (str.size() == 1)
+		{
+			str = "0" + str;
+		}
+		str += " / ";
+		std::string strMes = std::to_string(mes);
+		if (strMes.size() == 1)
+		{
+			strMes = "0" + strMes;
+		}
+		str += strMes;
+		str += " / ";
+		str += std::to_string(anio);
+		return str;
+	}
+
+	std::string toStringMes(int i) {
+		switch (i) {
+		case 1:
+			return "Enero";
+			break;
+		case 2:
+			return "Febrero";
+			break;
+		case 3:
+			return "Marzo";
+			break;
+		case 4:
+			return "Abril";
+			break;
+		case 5:
+			return "Mayo";
+			break;
+		case 6:
+			return "Junio";
+			break;
+		case 7:
+			return "Julio";
+			break;
+		case 8:
+			return "Agosto";
+			break;
+		case 9:
+			return "Septiembre";
+			break;
+		case 10:
+			return "Octubre";
+			break;
+		case 11:
+			return "Noviembre";
+			break;
+		case 12:
+			return "Diciembre";
+			break;
+
+		default:
+			break;
+		}
+	}
 };
