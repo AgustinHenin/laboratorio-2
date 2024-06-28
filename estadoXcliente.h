267ed6c1b04a4475b6118af4c4d7b4c1fe772eef
AgustinHenin <168901916+AgustinHenin@users.noreply.github.com>
2024-06-27 21:53:48 -0300

    Update estadoXcliente.h

--- estadoXcliente.h
+++ estadoXcliente.h
@@ -16,8 +16,8 @@ public:
     void mostrar() {}
 
 
-    int getidEstado() { return idEstado; }
-    int getidCliente() { return idCliente; }
+    int getidEstado() const { return idEstado; } //agrego const para que compile
+    int getidCliente() const { return idCliente; } //lo mismo
 
     void setidEstado(int e) { idEstado = e; }
     void setidCliente(int c) { idCliente = c; }
